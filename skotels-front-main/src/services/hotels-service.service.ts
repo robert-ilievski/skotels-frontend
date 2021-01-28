26f1f82eaf90762da4e87a27231fe7cad6198ad8
Robert Ilievski <rilievski54@gmail.com>
2021-01-28 20:20:17 +0100

    Updated the frontend

--- skotels-front-main/src/services/hotels-service.service.ts
+++ skotels-front-main/src/services/hotels-service.service.ts
@@ -10,24 +10,16 @@ import {HotelsModel} from '../models/hotels.model';
 export class HotelsServiceService {
   private hotelsUrl: string;
   constructor(private http: HttpClient) {
-    this.hotelsUrl = 'https://skotels2.herokuapp.com/api/hotels';
+    // this.hotelsUrl = 'https://skotels-gateway.herokuapp.com/api/hotels';
+    this.hotelsUrl = 'http://localhost:8080/api/hotels';
   }
   public add(hotel: HotelsModel): Observable<HotelsModel> {
     return this.http.post<HotelsModel>(`${this.hotelsUrl}/save`, hotel);
   }
   public findAll(): Observable<HotelsModel[]> {
-    return this.http.get<HotelsModel[]>(this.hotelsUrl);
-  }
-  // edit
-  public editHotel(data: HotelsModel, id: string): Promise<HotelsModel> {
-    return this.http
-      .put<HotelsModel>(`${this.hotelsUrl}/edit?id=${id}`, data)
-      .toPromise();
+    return this.http.get<HotelsModel[]>(`${this.hotelsUrl}/all`);
   }
   // delete
-  // deleteHotel(id: string): Promise<HotelsModel> {
-  //   return this.http.delete<HotelsModel>(`${this.hotelsUrl}/delete?id=${id}`).toPromise() ;
-  // }
   deleteHotel(hotel: HotelsModel): Observable<HotelsModel[]> {
     return this.http.post<HotelsModel[]>(`${this.hotelsUrl}/delete`, hotel);
   }
@@ -36,10 +28,8 @@ export class HotelsServiceService {
     return this.http.get<HotelsModel[]>(`${this.hotelsUrl}/sortbystars`);
   }
 
-  public sortByPrice(): Observable<HotelsModel[]> {
-    return this.http.get<HotelsModel[]>(`${this.hotelsUrl}/sortbyprice`);
+  public sortAlphabetic(): Observable<HotelsModel[]> {
+    return this.http.get<HotelsModel[]>(`${this.hotelsUrl}/sortalphabetic`);
   }
-  // search
-  // AIzaSyDB4R7MbHUmwlExVnCjwinN4xzzGd4-C14
 
 }
